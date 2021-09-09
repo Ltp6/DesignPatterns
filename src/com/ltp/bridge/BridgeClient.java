6e96623086558e3be21cc08a11c6076d771e2992
Ltp <18502825783@163.com>
2021-09-09 23:53:22 +0800

    :bug: 完善桥接模式代码

--- src/com/ltp/bridge/BridgeClient.java
+++ src/com/ltp/bridge/BridgeClient.java
@@ -9,15 +9,15 @@ public class BridgeClient {
 
     public static void main(String[] args) {
         System.out.println("=========直立小米手机========");
-        UprightPhone xiaoMiUpright = new UprightPhone(new XiaoMiPhone());
+        AbstractStyle xiaoMiUpright = new UprightPhone(new XiaoMiPhone());
         xiaoMiUpright.call();
 
         System.out.println("=========直立Vivo手机========");
-        UprightPhone vivoUpright = new UprightPhone(new VivoPhone());
+        AbstractStyle vivoUpright = new UprightPhone(new VivoPhone());
         vivoUpright.call();
 
         System.out.println("=========折叠Vivo手机========");
-        FoldPhone vivoFold = new FoldPhone(new VivoPhone());
+        AbstractStyle vivoFold = new FoldPhone(new VivoPhone());
         vivoFold.call();
     }
 }
